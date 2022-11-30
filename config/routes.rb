525dd2a76f04a06c7340af29f34a621048bfb560
narukawamasa <117150308+narukawamasa@users.noreply.github.com>
2022-11-30 05:05:07 +0000

    各controller作成復習

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+
+
+
   devise_for :users
+
+  resources :books, only: [:index, :show, :edit]
+  resources :users, only: [:index, :show, :edit]
+
+  root to: "homes#top"
+  get 'home/about' => 'homes#about', as: 'about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
