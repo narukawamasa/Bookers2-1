e0302f79c719f717dbab23591ddfe2769c435b62
narukawamasa <117150308+narukawamasa@users.noreply.github.com>
2022-11-30 12:28:48 +0000

    アクセス制限復習

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :is_matching_login_user, only: [:edit, :update]
+
   def index
     @book = Book.new
     @users = User.all
@@ -28,6 +30,14 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:name, :profile_image, :introduction)
+  end
+
+  def is_matching_login_user
+    user_id = params[:id].to_i
+    login_user_id = current_user.id
+    if(user_id != login_user_id)
+      redirect_to user_path(current_user.id)
+    end
   end
 
 end
